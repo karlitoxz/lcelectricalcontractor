051a79180ce69c5d0d54a196b92712ed949fd74b
karlitoxz <electronicaysistemas@gmail.com>
2015-11-06 16:01:29 -0500

    centar correo
    centar correo

---
+++ index.temp.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<title>lcelectricalcontractor.com
+		</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
+		<link href="assets/css/bootstrap.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+				<br>
+					<center><img src="images/lcelectricalcontractor.png"></center>
+					</div>
+					<div class="col-md-12">
+					<h3><center>Estamos en construcción, pero déjanos un mensaje.</center></h3>
+					</div>
+					<div class="col-md-12">
+					<h3><center>We are under construction, but leave us a message.</center></h3>
+					</div>
+			</div>	
+		</div>				
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+				</div>
+				
+					<div class="col-md-6">
+						
+						<form method="GET" action="email.php" id="formulario">
+							<div class="">
+								<div class="form-group"><input type="text" name="nombre" class="form-control" id="text" placeholder="Nombre"></div>
+								
+								<div class="form-group">
+															<select name="dirigido" class="form-control">
+																	<option value="default">Dirigido a: ?</option>
+																	<option value="ventas">Ventas</option>
+																	<option value="administracion">Administración</option>
+																	<option value="contabilidad">Contabilidad</option>
+																	<option value="web_master">Web Master</option>
+															</select>
+								</div>
+								
+								
+								<div class="form-group"><input class="form-control" type="email" name="email" id="email" placeholder="Escriba su dirección de correo electrónico"></div>
+								<div class="form-group"><textarea class="form-control" name="comentarios" maxlength="500" cols="50" rows="4" placeholder="Escriba sus comentarios"></textarea></div>
+								<div class=""><input class="btn btn-primary form-control" type="submit" value="Enviar" class="fit special"></div>
+							</div>
+						</form>
+					</div>	
+				<div class="col-md-3">
+				</div>			
+			</div>
+		</div>
+		
+
+	</body>
+</html>
